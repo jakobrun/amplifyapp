aaa1a32a2cd9c3560f9baf511549ce332eb1ff6f
jakob <jakob.runarsson@gmail.com>
2021-08-11 16:35:41 +0000

    update title

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+      <h1>Notes App</h1>
       <input
         onChange={(e) => setFormData({ ...formData, name: e.target.value })}
         placeholder="Note name"
